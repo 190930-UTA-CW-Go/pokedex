34d41a77c97b3c2d0e333fc8e5f16220b0261f5d
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-10 11:03:23 -0500

    adding pokemon_test

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"os"
+	"strconv"
+)
+
 func main() {
 	pokedex := Pokedex{}
 	bulbasaur := NewPokemon(1, "Bulbasaur", true)
 	pokedex.Add(bulbasaur)
+
+	if len(os.Args) > 1 {
+		id, _ := strconv.Atoi(os.Args[1])
+		name := os.Args[2]
+		var caught bool
+		if os.Args[3] == "true" {
+			caught = true
+		} else {
+			caught = false
+		}
+		newP := NewPokemon(id, name, caught)
+		pokedex.Add(newP)
+	}
+
 	pokedex.List()
 }
